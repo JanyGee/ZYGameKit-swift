2d8933c6104d7d3133da6ad984001108ff855c45
JanyGee <1321899953@qq.com>
2020-06-16 16:09:03 +0800

    提交sdk

---
+++ ZYWebGameKit.framework/Headers/ZYWebGameKit.h
@@ -0,0 +1,19 @@
+//
+//  ZYWebGameKit.h
+//  ZYWebGameKit
+//
+//  Created by admin on 2020/3/16.
+//  Copyright © 2020 Octopus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZYWebGameKit.
+FOUNDATION_EXPORT double ZYWebGameKitVersionNumber;
+
+//! Project version string for ZYWebGameKit.
+FOUNDATION_EXPORT const unsigned char ZYWebGameKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZYWebGameKit/PublicHeader.h>
+
+
